fcdb212635d3541a7cfcb5dbf7d7be995e34158b
hoangtn <hoangtn@onextdigital.com>
2024-07-16 14:00:01 +0700

    update

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import chatRoutes from '../routes/chatRoutes.js';
+import config from '../config/config.js';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+app.use('/api', chatRoutes);
+
+app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+});
